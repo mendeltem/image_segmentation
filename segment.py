ea885f91e2c0cd784f318e3fa9748a40b8aa1112
Your Name <you@example.com>
2019-01-11 13:03:08 +0100

    programmed py

---
+++ segment.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Mask R-CNN Demo
+# 
+# A quick intro to using the pre-trained model to detect and segment objects.
+
+# In[1]:
+
+
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+# Import COCO config
+sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
+
+import cv2
+import coco
+
+
+# In[20]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+
+# ## Configurations
+# 
+# We'll be using a model trained on the MS-COCO dataset. The configurations of this model are in the ```CocoConfig``` class in ```coco.py```.
+# 
+# For inferencing, modify the configurations a bit to fit the task. To do so, sub-class the ```CocoConfig``` class and override the attributes you need to change.
+
+# In[10]:
+
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+#config.display()
+
+
+# ## Create Model and Load Trained Weights
+
+# In[6]:
+
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+
+# ## Class Names
+# 
+# The model classifies objects and returns class IDs, which are integer value that identify each class. Some datasets assign integer values to their classes and some don't. For example, in the MS-COCO dataset, the 'person' class is 1 and 'teddy bear' is 88. The IDs are often sequential, but not always. The COCO dataset, for example, has classes associated with class IDs 70 and 72, but not 71.
+# 
+# To improve consistency, and to support training on data from multiple sources at the same time, our ```Dataset``` class assigns it's own sequential integer IDs to each class. For example, if you load the COCO dataset using our ```Dataset``` class, the 'person' class would get class ID = 1 (just like COCO) and the 'teddy bear' class is 78 (different from COCO). Keep that in mind when mapping class IDs to class names.
+# 
+# To get the list of class names, you'd load the dataset and then use the ```class_names``` property like this.
+# ```
+# # Load COCO dataset
+# dataset = coco.CocoDataset()
+# dataset.load_coco(COCO_DIR, "train")
+# dataset.prepare()
+# 
+# # Print class names
+# print(dataset.class_names)
+# ```
+# 
+# We don't want to require you to download the COCO dataset just to run this demo, so we're including the list of class names below. The index of the class name in the list represent its ID (first class is 0, second is 1, third is 2, ...etc.)
+
+# In[7]:
+
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+
+# ## Run Object Detection
+
+# In[ ]:
+
+
+# Load a random image from the images folder
+file_names = next(os.walk(IMAGE_DIR))[2]
+image = skimage.io.imread(os.path.join(IMAGE_DIR, file_names[0]))
+
+# Run detection
+results = model.detect([image], verbose=1)
+r = results[0]
+#plt.imshow(r['masks'][:,:,0])
+
+
+print(r['scores'])
+print(r.keys())
+
+j =2
+
+print("shape ",r['masks'].shape)
+
+blue  =  r['masks'][:,:,j]
+green =  r['masks'][:,:,j]
+red   =  r['masks'][:,:,j]
+"""Create a blank image that has three channels 
+and the same number of pixels as your original input"""
+needed_multi_channel_img = np.zeros((red.shape[0], red.shape[1], 3))
+
+"""Add the channels to the needed image one by one"""
+needed_multi_channel_img [:,:,0] = green
+needed_multi_channel_img [:,:,1] = red
+needed_multi_channel_img [:,:,2] = blue
+
+new = (needed_multi_channel_img*image).astype(np.uint8)
+plt.imshow(new)
+
+
+# In[17]:
+
+
+import matplotlib.image as mpimg
+
+#img = mpimg.imread("src.png")
+mpimg.imsave("../output/out.png", new)
+
+
+# In[ ]:
+
+
+plt.imshow(image)
+
+
+# In[413]:
+
+
+# Visualize results
+r = results[0]
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+                            class_names, r['scores'])
+
